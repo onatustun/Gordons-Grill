36d59368e5c62eb38f7899c72fae511b857fbe74
Onat Ustun <onatustun@gmail.com>
2024-10-02 19:15:09 +0100

    suff

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,10 @@ module.exports = {
       },
       backdropBlur: {
         xs: '2px',
-      }
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
     },
   },
   variants: {},
