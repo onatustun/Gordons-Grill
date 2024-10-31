d289d8abb536aae55a73b680d8e5bc133676050b
Onat Ustun <onatustun@gmail.com>
2024-10-31 21:29:58 +0000

    ADD: hamburger click/curtain menu

--- dist/js/navScroll.js
+++ dist/js/navScroll.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
     let prevScrollPos = $(window).scrollTop()
     const navbar = $("nav")
-    const navBg = $(".navBg")
+    const hamburger = $(".hamburger")
+    const body = $("body")
+    let curtainOpen = false
 
     $(window).on('scroll', () => {
         const currentScrollPos = $(window).scrollTop()
@@ -9,8 +11,27 @@ $(document).ready(() => {
         navbar.css('top', prevScrollPos > currentScrollPos ? '0' : '-6.5rem')
 
         navbar.toggleClass('bg-black', currentScrollPos > $(window).height() - 88)
-        navbar.toggleClass('bg-none', currentScrollPos <= $(window).height() - 88)
 
         prevScrollPos = currentScrollPos
+    })
+
+    hamburger.on("click", () => {
+        const currentScrollPos = $(window).scrollTop()
+        if (curtainOpen) {
+            navbar.removeClass('h-screen')
+            if (currentScrollPos < $(window).height() - 88) {
+                navbar.removeClass('bg-black')
+            }
+        } else {
+            navbar.addClass('bg-black h-screen')
+        }
+
+        if ($(window).width() <= 1024 && navbar.hasClass('h-screen')) {
+            body.addClass('overflow-y-hidden')
+        } else {
+            body.removeClass('overflow-y-hidden')
+        }
+
+        curtainOpen = !curtainOpen
     })
 })
