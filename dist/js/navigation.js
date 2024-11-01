c93f9d3d420f3255e7c8cf5b380b3fea93021ffa
Onat Ustun <onatustun@gmail.com>
2024-11-01 10:12:45 +0000

    ADD: curtain content

--- dist/js/navigation.js
+++ dist/js/navigation.js
@@ -1,47 +1,56 @@
 $(document).ready(() => {
-    let prevScrollPos = $(window).scrollTop()
-    const navbar = $("nav")
-    const hamburger = $(".hamburger")
-    const body = $("body")
-    let curtainOpen = false
+  let prevScrollPos = $(window).scrollTop();
+  const navbar = $("nav");
+  const hamburger = $(".hamburger");
+  const curtainContent = $(".curtain-content");
+  const body = $("body");
+  let curtainOpen = false;
 
-    $(window).on('scroll', () => {
-        const currentScrollPos = $(window).scrollTop()
+  $(window).on("scroll", () => {
+    const currentScrollPos = $(window).scrollTop();
 
-        navbar.toggleClass('top-0', prevScrollPos > currentScrollPos).toggleClass('-top-26', !(prevScrollPos > currentScrollPos))
+    navbar
+      .toggleClass("top-0", prevScrollPos > currentScrollPos)
+      .toggleClass("-top-26", !(prevScrollPos > currentScrollPos));
 
-        navbar.toggleClass('bg-black', currentScrollPos > $(window).height() - 88)
+    navbar.toggleClass("bg-black", currentScrollPos > $(window).height() - 88);
 
-        prevScrollPos = currentScrollPos
-    })
+    prevScrollPos = currentScrollPos;
+  });
 
-    hamburger.on("click", () => {
-        const currentScrollPos = $(window).scrollTop()
+  hamburger.on("click", () => {
+    const currentScrollPos = $(window).scrollTop();
 
-        if (curtainOpen) {
-            navbar.removeClass('h-screen')
-            if (currentScrollPos < $(window).height() - 88) {
-                navbar.removeClass('bg-black')
-            }
-        } else {
-            navbar.addClass('bg-black h-screen')
-        }
+    if (curtainOpen) {
+      navbar.removeClass("h-screen");
+      if (currentScrollPos < $(window).height() - 88) {
+        navbar.removeClass("bg-black");
+      }
+    } else {
+      navbar.addClass("bg-black h-screen");
+    }
 
-        body.toggleClass('overflow-y-hidden', $(window).width() < 768 && navbar.hasClass('h-screen'))
+    curtainContent.toggleClass("invisible");
 
-        curtainOpen = !curtainOpen
-    })
+    body.toggleClass(
+      "overflow-y-hidden",
+      $(window).width() < 768 && navbar.hasClass("h-screen"),
+    );
 
-    $(window).on('resize', () => {
-        const currentScrollPos = $(window).scrollTop()
+    curtainOpen = !curtainOpen;
+  });
 
-        if ($(window).width() >= 768 && curtainOpen) {
-            navbar.removeClass('h-screen')
-            if (currentScrollPos < $(window).height() - 88) {
-                navbar.removeClass('bg-black')
-            }
-            body.removeClass('overflow-y-hidden')
-            curtainOpen = false
-        }
-    })
-})
+  $(window).on("resize", () => {
+    const currentScrollPos = $(window).scrollTop();
+
+    if ($(window).width() >= 768 && curtainOpen) {
+      navbar.removeClass("h-screen");
+      curtainContent.addClass("invisible");
+      if (currentScrollPos < $(window).height() - 88) {
+        navbar.removeClass("bg-black");
+      }
+      body.removeClass("overflow-y-hidden");
+      curtainOpen = false;
+    }
+  });
+});
